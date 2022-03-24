96293ebb17c92bfacddbb533f3991311447520fe
Frans de Boer <frans-de-boer@hotmail.com>
2022-03-24 11:25:37 +0100

    created auto runner

---
+++ runner.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+/home/frans/Projects/aidm-assignment
+echo 1
+minizinc --solver gecode model_2.mzn instances/Example1.dzn --time-limit 3600000 --output-time
+echo 2
+minizinc --solver gecode model_2.mzn instances/Example2.dzn --time-limit 3600000 --output-time
+#echo 3
+#minizinc --solver gecode model_2.mzn instances/Example3.dzn --time-limit 3600000 --output-time
+echo 4
+minizinc --solver gecode model_2.mzn instances/Example4.dzn --time-limit 3600000 --output-time
+echo 5
+minizinc --solver gecode model_2.mzn instances/Example5.dzn --time-limit 3600000 --output-time
+echo 6
+minizinc --solver gecode model_2.mzn instances/Example6.dzn --time-limit 3600000 --output-time
+echo 7
+minizinc --solver gecode model_2.mzn instances/Example7.dzn --time-limit 3600000 --output-time
+echo 8
+minizinc --solver gecode model_2.mzn instances/Example8.dzn --time-limit 3600000 --output-time
+echo 9
+minizinc --solver gecode model_2.mzn instances/Example9.dzn --time-limit 3600000 --output-time
+echo 10
+minizinc --solver gecode model_2.mzn instances/Example10.dzn --time-limit 3600000 --output-time
+echo 11
+minizinc --solver gecode model_2.mzn instances/Example11.dzn --time-limit 3600000 --output-time
+echo 12
+minizinc --solver gecode model_2.mzn instances/Example12.dzn --time-limit 3600000 --output-time
+echo 13
+minizinc --solver gecode model_2.mzn instances/Example13.dzn --time-limit 3600000 --output-time
+echo 14
+minizinc --solver gecode model_2.mzn instances/Example14.dzn --time-limit 3600000 --output-time
+echo 15
+minizinc --solver gecode model_2.mzn instances/Example15.dzn --time-limit 3600000 --output-time
+echo 16
+minizinc --solver gecode model_2.mzn instances/Example16.dzn --time-limit 3600000 --output-time
+echo 17
+minizinc --solver gecode model_2.mzn instances/Example17.dzn --time-limit 3600000 --output-time
+echo 18
+minizinc --solver gecode model_2.mzn instances/Example18.dzn --time-limit 3600000 --output-time
+echo 19
+minizinc --solver gecode model_2.mzn instances/Example19.dzn --time-limit 3600000 --output-time
+echo 20
+minizinc --solver gecode model_2.mzn instances/Example20.dzn --time-limit 3600000 --output-time
